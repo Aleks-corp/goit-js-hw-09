b0b0d8c49abcc50de7a69d1923990a484ddaaf1b
Aleks Haran <nporock@gmail.com>
2022-09-23 23:32:30 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
+}
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  let delayTime = Number(delay.value);
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    createPromise(i, delayTime)
+      .then(res => {
+        Notify.success(res);
+      })
+      .catch(res => {
+        Notify.failure(res);
+      });
+
+    delayTime += Number(step.value);
   }
 }
